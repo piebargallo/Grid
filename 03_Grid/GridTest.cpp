8a445c71019a31a69b706f1d1d503e1b5b78d262
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-06-23 10:58:18 +0200

    Add files via upload

---
+++ 03_Grid/GridTest.cpp
@@ -0,0 +1,21 @@
+import grid;
+#include <iostream>
+using std::cout;
+
+constexpr size_t getHeight()
+{
+	return 10;
+}
+
+int main()
+{
+	Grid<int, 10, 10> myGrid;
+	Grid<int, 10, 10> anotherGrid;
+
+	myGrid.at(2, 3) = 42;
+	anotherGrid = myGrid;
+
+	cout << anotherGrid.at(2, 3).value_or(0);
+
+	Grid<double, 2, getHeight()> myDoubleGrid;
+}
