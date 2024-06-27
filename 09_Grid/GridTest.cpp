86ee5eadc1963b1fbcf603e1d5780dd7b4d43057
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-06-27 16:36:54 +0200

    Add files via upload

---
+++ 09_Grid/GridTest.cpp
@@ -0,0 +1,20 @@
+import grid;
+#include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
+
+namespace {
+	int defaultInt{ 11 };
+}
+
+int main()
+{
+	Grid<int, defaultInt> myIntGrid;
+	Grid<int, defaultInt> myIntGrid2{ myIntGrid };
+	myIntGrid2.at(1, 2) = 3;
+
+	cout << myIntGrid.at(1, 2).value_or(-1) << endl;
+	cout << myIntGrid2.at(1, 2).value_or(-1) << endl;
+}
